f55d753350d1188fc51b33440490aab42fa6c7d5
Guilherme Queiroz <guiqueiroz02.gq@gmail.com>
2022-02-10 17:49:46 -0300

    First commit

---
+++ app/src/main/java/com/example/gastosdeviagem02/DataEntryActivity.kt
@@ -0,0 +1,53 @@
+package com.example.gastosdeviagem02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_data_entry.*
+import java.lang.NumberFormatException
+
+class DataEntryActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_entry)
+
+        supportActionBar?.hide()
+
+        buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.buttonCalculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        if (validationOK()) {
+
+            try {
+                val distance = editDistance.text.toString().toFloat()
+                val price = editPrice.text.toString().toFloat()
+                val autonomy = editAutonomy.text.toString().toFloat()
+
+                val totalValue = (distance * price) / autonomy
+                textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+
+            } catch (nfe: NumberFormatException) {
+                Toast.makeText(this, getString(R.string.valores_validos), Toast.LENGTH_LONG).show()
+            }
+
+        } else {
+            Toast.makeText(this, getString(R.string.preencha_todos_campos), Toast.LENGTH_LONG)
+                .show()
+        }
+
+    }
+
+    private fun validationOK(): Boolean {
+        return (editDistance.text.toString() != "" && editPrice.text.toString() != "" && editAutonomy.text.toString() != "")
+
+    }
+}
